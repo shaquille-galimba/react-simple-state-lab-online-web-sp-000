aa0549a5f9cfd32d9d3ffad52ba43c5f8deafe07
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-10-29 02:52:20 -0400

    Done.

---
+++ src/Cell.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+export default class Cell extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			color: this.props.value
+		}
+	}
+
+	handleClick = () => {
+		this.setState({
+			color: '#333'
+		})
+	}
+
+	render() {
+		return (
+
+			<div
+				className="cell"
+				style={{backgroundColor: this.state.color}}
+				onClick= {this.handleClick}>
+
+			</div>
+		)
+	}
+}
+
+Cell.defaultProps = {
+	value: '#F00'
+}
